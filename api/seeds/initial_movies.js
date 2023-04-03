54a8c7218b89d88a9831944d9cd31ac268691429
Yeehowdy <kylemersinger67@gmail.com>
2023-04-03 17:03:49 +0200

    API stuff

---
+++ api/seeds/initial_movies.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  await knex('movies').del()
+  await knex('movies').insert([
+    {title: "Kung Fury"},
+    {title: "Nacho Libre"},
+    {title: "Pulp Fiction"},
+    {title: "Idiocracy"},
+    {title: "Dark Tower"},
+    {title: "WALL-E"},
+    {title: "Alien"},
+    {title: "Sharknado"},
+    {title: "Coco"},
+    {title: "Soul"},
+    {title: "Baby Driver"},
+    {title: "Raiders oof the Lost Ark"},
+  ]);
+};
